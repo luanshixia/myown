14329c9e357e0a00f38850b996ee841bab26b45e
luanshixia <dreambuild@DreamMac.local>
2017-05-13 22:21:52 -0700

    Added google and maps

--- index.js
+++ index.js
@@ -8,12 +8,31 @@ const program = require('commander');
 const open = require('open');
 
 program
-  .version('0.0.1');
+  .version('0.0.1')
+  .usage('<command> [options] <argument>');
 
 program
-  .usage('browse <url>')
-  .action((cmd, url) => {
+  .command('browse <url>')
+  .action((url) => {
     console.log(`Opening ${url} in browser...`);
+    open(url);
+  });
+
+program
+  .command('google <kwd>')
+  .action((kwd) => {
+    kwd = encodeURIComponent(kwd);
+    const url = `https://www.google.com/#q=${kwd}`;
+    console.log('Opening Google in browser...');
+    open(url);
+  });
+
+program
+  .command('maps <kwd>')
+  .action((kwd) => {
+    kwd = encodeURIComponent(kwd);
+    const url = `https://www.google.com/maps/search/${kwd}`;
+    console.log('Opening Google Maps in browser...');
     open(url);
   });
 
